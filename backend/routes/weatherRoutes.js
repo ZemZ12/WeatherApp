cdf0c7a1816b77098c5d83a3a57cece3a02bb026
RBeech03 <rymy2003@gmail.com>
2025-03-30 18:44:16 -0400

    Update of backend to allow for databasing with weather

--- backend/routes/weatherRoutes.js
+++ backend/routes/weatherRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 require('dotenv').config();
+const authenticateToken = require("../middleware/auth");
+const Location = require('../models/Locations');
 
 const apiKey = process.env.OPENWEATHERMAP_API_KEY;
 
@@ -33,6 +35,29 @@ router.get('/currentWeather', async (req, res) => {
         res.status(500).json( {error: error.message});
     }
 });
+
+router.post("/addLocation", authenticateToken, async (req, res) => {
+    try {
+        const {city} = req.body;
+        const userId = req.user.userId;
+        const newLocation = new Location({city, userId});
+        await newLocation.save();
+        res.status(201).json({ message: "Location Saved"});
+    } catch (err){
+        res.status(500).json({message : "Error Saving Location"});
+    }
+});
+
+router.get("/getLocations", authenticateToken, async (req, res) => {
+    const userId = req.user.userId;
+  
+    try {
+      const locations = await Location.find({ userId });
+      res.status(200).json(locations);
+    } catch (err) {
+      res.status(500).json({ message: "Error fetching locations" });
+    }
+  });
 
 
 module.exports = router;
