3be9efbe70dd514d6ce39e90bb06dfe8879486c9
RBeech03 <rymy2003@gmail.com>
2025-04-07 16:27:45 -0400

    Added reverseGeocoding for automatic user location weather.

--- backend/routes/weatherRoutes.js
+++ backend/routes/weatherRoutes.js
@@ -12,7 +12,7 @@ router.get('/weeklyForcast', async (req, res) => {
 	try{
         const {city} = req.body;
 	
-	    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+	    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
 	    const response = await fetch(url);
 	    const data = await response.json();
 
@@ -26,7 +26,7 @@ router.get('/weeklyForcast', async (req, res) => {
 router.get('/currentWeather', async (req, res) => {
     try {
         const {city} = req.body;
-        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
         const response = await fetch(url);
         const data = await response.json();
         
@@ -80,6 +80,21 @@ router.delete("/deleteLocation/:id", authenticateToken, async (req, res) => {
         res.status(500).json({ message: "Server error"});
     }
 });
+
+router.post("/reverseGeocoding", async (req, res) => {
+    try {
+        const {lon, lat} = req.body;
+        const url = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`;
+        response = await fetch(url);
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch(error) {
+        console.error(error);
+        res.status(500).json( {error: error.message})
+    }
+});
+
+
 
 
 module.exports = router;
