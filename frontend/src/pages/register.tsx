a7deedb6568bf7bfa82b58cdfa6636b05da4a853
Caden Peterson <57927858+c4den@users.noreply.github.com>
2025-04-10 19:44:02 -0400

    Fixed it! Localhost works now :)

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -61,7 +61,7 @@ const Register = () => {
     if (!validateForm()) return;
 
     try {
-      const response = await fetch("https://weatherApp46.xyz/api/register", {
+      const response = await fetch("/api/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email: formData.email, password: formData.password }),
